022e0f681ccd45a9b166fff9ce93c01e96e9b89c
tejas <54097365+tejxv@users.noreply.github.com>
2020-05-25 16:30:25 +0530

    colorful shiz

--- main.js
+++ main.js
@@ -1,30 +1,72 @@
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-
-let darkMode = localStorage.getItem('darkMode');
-
-if (darkMode === 'enabled') {
-  	enableDarkMode();
-} else {
-	disableDarkMode();
-}
-
-darkModeToggle.addEventListener('click', () => {
-  darkMode = localStorage.getItem('darkMode'); 
-  if (darkMode !== 'enabled') {
-    enableDarkMode();  
-  } else {  
-    disableDarkMode(); 
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '~<>-_\\/[]{}—=+*^?#_______'
+    this.update = this.update.bind(this)
   }
-});
-
-function enableDarkMode() {
-   var element = document.body;
-   element.classList.add("dark-mode");
-   localStorage.setItem('darkMode', 'enabled');
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 50)
+      const end = start + Math.floor(Math.random() * 100)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.01) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
 }
 
-function disableDarkMode() {
-   var element = document.body;
-   element.classList.remove("dark-mode");
-   localStorage.setItem('darkMode', null );
+
+const phrases = [
+  '_',
+  'Tejas',
+]
+
+const el = document.querySelector('.name')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 3000)
+  })
+  counter = (counter + 1) % phrases.length
 }
+
+next()
