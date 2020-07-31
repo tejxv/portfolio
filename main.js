fae6d9ff9a2a99e5dbaafe9f1a14b487389af423
tejas <tejxv@users.noreply.github.com>
2020-07-31 18:21:26 +0530

    🎶 Spotify currently streaming

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// onLoad Animation
-
-window.onload = function () {
-  var box = document.getElementById("box");
-  box.style.animation = "anim 800ms cubic-bezier(1,1,.49,1.05)";
-  box.style.opacity = 1;
-};
-
 // Text Scramble Fx
 
 class TextScramble {
@@ -76,3 +68,59 @@ const next = () => {
 };
 
 next();
+
+// spotify activity
+
+const trackArtist = document.querySelector("#trackartist");
+const trackName = document.querySelector("#tracktitle");
+const nowPlaying = document.querySelector("#nowplaying");
+const trackArtwork = document.querySelector("#trackartwork");
+const shadowArtwork = document.querySelector("#shadowartwork");
+const timestamp = document.querySelector("#timestamp");
+const lastfm = document.querySelector("#lastfm");
+
+const track = {};
+
+function latestTrack() {
+  let api =
+    "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=lltejasll&format=json&api_key=0d18c849abdeb5bb7df9437a19e70114&limit=1";
+  fetch(api)
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      track.artist = data["recenttracks"]["track"][0]["artist"]["#text"];
+      track.name = data["recenttracks"]["track"][0]["name"];
+      track.artwork = data["recenttracks"]["track"][0]["image"][3]["#text"];
+      if ("@attr" in data["recenttracks"]["track"][0]) {
+        track.nowPlaying = true;
+      } else {
+        track.nowPlaying = false;
+        console.log("not currently playing");
+        track.time = data["recenttracks"]["track"][0]["date"]["uts"];
+        console.log(track.time);
+      }
+    })
+    .then(function () {
+      displayTrack();
+    });
+}
+
+function displayTrack() {
+  trackArtist.innerHTML = `${track.artist}`;
+  trackName.innerHTML = `${track.name}`;
+  trackArtwork.src = `${track.artwork}`;
+  shadowArtwork.src = `${track.artwork}`;
+  track.time = `${track.time}`;
+
+  if (track.nowPlaying) {
+    nowPlaying.innerHTML = `currently listening`;
+  } else {
+    timestamp.innerHTML = `listened ` + moment.unix(`${track.time}`).fromNow();
+    nowPlaying.style.display = "none";
+  }
+}
+
+latestTrack();
+setInterval(latestTrack, 5000);
